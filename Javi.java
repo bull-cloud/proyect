9a757922af61a6b3fa8290a1285fef9e0f25f741
mono <javijks@gmail.com>
2025-04-03 17:21:05 +0200

    git s

--- Javi.java
+++ Javi.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo!");
-        //System.out.println("¡Adios mundo!");
+        System.out.println("¡Adidfshsg!");
         
     }
 }
