35d93f242498114fc86a9664fa905953df7cca39
mono <javijks@gmail.com>
2025-04-03 17:25:14 +0200

    estoy aqui

--- Javi.java
+++ Javi.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo!");
         System.out.println("¡Adidfshsg!");
-        
+        int a=22;
     }
 }
