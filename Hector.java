934a08e62e73d4362d489f08fc9f5f1d616fd259
hectorgar2000 <hectorgar2000@gmail.com>
2025-04-10 16:43:45 +0200

    Create Hector.java

---
+++ Hector.java
@@ -0,0 +1,48 @@
+
+import java.time.LocalDate;
+
+public class Diario {
+	private String Actividades;
+	private String dificultdes;
+	private String herramientas;
+	private LocalDate fechaCambio;
+
+Diario(){
+	
+}
+
+public String getActividades() {
+	return Actividades;
+}
+
+public void setActividades(String actividades) {
+	Actividades = actividades;
+}
+
+public String getDificultdes() {
+	return dificultdes;
+}
+
+public void setDificultdes(String dificultdes) {
+	this.dificultdes = dificultdes;
+}
+
+public String getHerramientas() {
+	return herramientas;
+}
+
+public void setHerramientas(String herramientas) {
+	this.herramientas = herramientas;
+}
+
+public LocalDate getFechaCambio() {
+	return fechaCambio;
+}
+
+public void setFechaCambio(LocalDate fechaCambio) {
+	this.fechaCambio = fechaCambio;
+}
+
+
+
+}
